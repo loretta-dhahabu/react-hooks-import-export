b67fd2e0c22a08661e0c861e43b7f3d1bcd899a0
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-13 09:15:20 +0300

    add national parks

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
+
+console.log( RMFunctions.trees );
+RMFunctions.wildlife();
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde/>
+    </div>
+  );
 }
+export default ColoradoStateParks;
